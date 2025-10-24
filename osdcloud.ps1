6367b31e9e738d03f5e5fbaf4124d1bc665efb3f
Jack Smith <jackniksmith@googlemail.com>
2025-10-24 16:01:51 +0100

    Update osdcloud.ps1

--- osdcloud.ps1
+++ osdcloud.ps1
@@ -1,6 +1,7 @@
 Write-Output "OSDCloud Imager"
 
 # ======= Parameters =======
+<#
 $OSVersion      = "24H2"                   # Windows version
 $OSName         = "11"                     # Major OS version
 $OSEdition      = "Pro"                    # Edition
@@ -12,7 +13,6 @@ $NoPrompt       = $true                     # Suppress all prompts
 
 # ======= Initialization =======
 Start-Sleep -Seconds 10
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
 # ======= Build Start-OSDCloud Parameters Dynamically =======
 $params = @{
@@ -28,3 +28,33 @@ if ($NoPrompt)   { $params.NoPrompt  = $true }
 
 # ======= Run OSDCloud =======
 Start-OSDCloud @params
+#>
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+
+#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+$OSName = 'Windows 11 23H2 x64'
+$OSEdition = 'Pro'
+$OSActivation = 'Retail'
+$OSLanguage = 'en-us'
+
+#Set OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$true
+    WindowsUpdateDrivers = [bool]$true
+    WindowsDefenderUpdate = [bool]$true
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$true
+    CheckSHA1 = [bool]$true
+}
+
+#Launch OSDCloud
+Write-Host "Starting OSDCloud" -ForegroundColor Green
+write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
