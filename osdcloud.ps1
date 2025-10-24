04980f67e2ead1fdcd57ad1e28206f7faf53d716
Jack Smith <jackniksmith@googlemail.com>
2025-10-24 15:56:58 +0100

    Update osdcloud.ps1

--- osdcloud.ps1
+++ osdcloud.ps1
@@ -4,10 +4,10 @@ $version = "24h2"
 $wipedisk = $true
 $os = "11"
 $OSEdition = "Pro"
-$cleardiskconfirm = $false
+$cleardiskconfirm = "-noprompt"
 
 sleep 20
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-Start-OSDCloud -OSName 'Windows 11 24h2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail -zti -wipedisk -cleardiskconfirm $cleardisk
+Start-OSDCloud -OSName 'Windows 11 24h2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail -zti -wipedisk $cleardisk
 
 sleep 20
