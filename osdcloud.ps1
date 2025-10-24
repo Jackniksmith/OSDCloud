cefacbca0ecc5a992f1362e1d1047f37fc08037a
Jack Smith <jackniksmith@googlemail.com>
2025-10-24 15:53:13 +0100

    Update osdcloud.ps1

--- osdcloud.ps1
+++ osdcloud.ps1
@@ -1,7 +1,12 @@
-write-output "testing testing 123"
+write-output "OSDCloud Imager"
+
+$version = "24h2"
+$wipedisk = $true
+$os = "11"
+$OSEdition "Pro"
 
 sleep 20
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-Start-OSDCloud -OSName 'Windows 11 24h2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail -zti
+Start-OSDCloud -OSName 'Windows 11 24h2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail -zti -wipedisk
 
 sleep 20
