98b4f93d302d8660a6f3398259b0f126e19e66b5
Jack Smith <jackniksmith@googlemail.com>
2025-10-24 15:49:39 +0100

    Update osdcloud.ps1

--- osdcloud.ps1
+++ osdcloud.ps1
@@ -1,7 +1,7 @@
 write-output "testing testing 123"
 
 sleep 20
-
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 Start-OSDCloud -OSName 'Windows 11 24h2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail
 
 sleep 20
